a6f163cee087b4eb04fb6771e8aed959992c992f
aarontravass <aaron.travass@hotmail.com>
2023-10-13 16:08:57 -0400

    chore: wip

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -4,7 +4,7 @@ import { FuzzyHttp } from '@fuzzymer/http'
 const cli = cac('fuzzymer')
 
 cli
-  .command('test <url>', 'Test a url')
+  .command('test <url>', 'Test a template url')
   .option('--exit-on-error', 'Exit on error')
   .action(async (url: string, options: Record<string, boolean>) => {
     const http = new FuzzyHttp({ exitOnError: !!options.exitOnError })
